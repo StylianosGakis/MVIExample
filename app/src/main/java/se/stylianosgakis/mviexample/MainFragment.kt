96eae8144a54d9ca927c12ea40d6fac99d912f3e
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2019-10-14 01:12:20 +0200

    Add main fragment basic initialization

---
+++ app/src/main/java/se/stylianosgakis/mviexample/MainFragment.kt
@@ -0,0 +1,37 @@
+package se.stylianosgakis.mviexample
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_get_user -> {
+                //TODO functionality for clicking get user menu option
+            }
+            R.id.action_get_blogs -> {
+                //TODO functionality for clicking get blogs menu option
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
