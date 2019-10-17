800e35bce402fed9e9f828ab2a1d2f7b76e300c0
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2019-10-17 05:36:09 +0200

    Add DataStateListener interface
    This class will be used to transfer information from fragments to
    activities. Particularly, information about the DataState and when it
    changes. This results in the activity knowing about if there is a
    message inside the data, because an error has happened for example
    which can be used for showing a Toast. And also, if the state is at a
    state of loading, therefore update the UI accordingly.

---
+++ app/src/main/java/se/stylianosgakis/mviexample/ui/DataStateListener.kt
@@ -0,0 +1,9 @@
+package se.stylianosgakis.mviexample.ui
+
+import se.stylianosgakis.mviexample.util.DataState
+
+interface DataStateListener {
+
+    fun onDataStateChange(dataState: DataState<*>)
+
+}
